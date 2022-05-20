3e02090ef05e2bc56ed540f0d07f10c73287254b
emrekcgll <emrekcgll@gmail.com>
2022-05-20 16:03:46 +0300

    .

---
+++ ocr/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using IronOcr;
+
+namespace ocr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int b = 1;
+            int dosyaSayisi = Directory.GetFiles("img/", "*.*", SearchOption.AllDirectories).Length;
+            var Ocr = new IronTesseract();
+            using (var input = new OcrInput())
+            { 
+                input.Title = "Quarterly Report";
+                for (int i = 0; i < dosyaSayisi; i++)
+                {
+                    input.AddImage("img/1 ("+b+").htm");
+                    b++;
+                }
+                var Result = Ocr.Read(input);
+                Result.SaveAsTextFile("AAAAA.txt");
+                Environment.Exit(0);
+            }
+            Environment.Exit(0);
+        }
+    }
+}
